e5ecb1047123c4a7dff0d2e013e83948644c185d
nelbol <gabriel_c_cordeiro@estudante.sc.senai.br>
2019-09-12 16:39:13 -0300

    Prepararação para gerar nova senha
    #12

--- 127_0_0_1.sql
+++ 127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 06-Set-2019 às 16:47
+-- Tempo de geração: 12-Set-2019 às 16:37
 -- Versão do servidor: 10.3.16-MariaDB
 -- versão do PHP: 7.3.7
 
@@ -21,6 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `sistemadelogin`
 --
+DROP DATABASE IF EXISTS `sistemadelogin`;
 CREATE DATABASE IF NOT EXISTS `sistemadelogin` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 USE `sistemadelogin`;
 
@@ -37,8 +38,17 @@ CREATE TABLE `usuario` (
   `email` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `senha` char(40) COLLATE utf8mb4_bin NOT NULL,
   `dataCriacao` datetime NOT NULL,
-  `avatar` varchar(200) COLLATE utf8mb4_bin NOT NULL
+  `avatar` varchar(200) COLLATE utf8mb4_bin NOT NULL,
+  `token` char(10) COLLATE utf8mb4_bin NOT NULL,
+  `tempo_de_vida` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
+INSERT INTO `usuario` (`idUsuario`, `nome`, `nomeUsuario`, `email`, `senha`, `dataCriacao`, `avatar`, `token`, `tempo_de_vida`) VALUES
+(1, 'gusta', 'gutano', 'malkete.chaves2911@gmail.com', '8c46c4c599cfb8ff2b7752c3de5b7f47c9a8653d', '2019-09-12 14:07:20', 'https://media2.giphy.com/media/Viql5vpvEwjug/source.gif', '', '2019-09-12 19:29:48');
 
 --
 -- Índices para tabelas despejadas
@@ -59,7 +69,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `idUsuario` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
